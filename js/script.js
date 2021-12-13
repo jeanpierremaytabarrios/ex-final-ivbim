d3f05bf3f1cb7ca6d34a6e614785fd9e1fa7338a
Jeanpierre Mayta <jeanpierremaytabarrios@gmail.com>
2021-12-13 12:45:38 -0500

    letter3

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ const generar = () => {
             show.lastElementChild.style.flexDirection = "row";
             show.lastElementChild.style.justifyContent = "center";
             show.lastElementChild.style.alignItems = "center";
-            show.lastElementChild.firstElementChild.style.fontSize = "2.5rem";
+            show.lastElementChild.firstElementChild.style.fontSize = "1rem";
             show.lastElementChild.firstElementChild.style.fontFamily = "Candara";
         }
         else if(ch_shape.value == "circulo")
@@ -39,7 +39,7 @@ const generar = () => {
             show.lastElementChild.style.flexDirection = "row";
             show.lastElementChild.style.justifyContent = "center";
             show.lastElementChild.style.alignItems = "center";
-            show.lastElementChild.firstElementChild.style.fontSize = "2.5rem";
+            show.lastElementChild.firstElementChild.style.fontSize = "1rem";
             show.lastElementChild.firstElementChild.style.fontFamily = "Candara";
         }
         ch_name.value = "";
